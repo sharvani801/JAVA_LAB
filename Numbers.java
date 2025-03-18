97d94b49cc821cdffdf4143c85f7c31f0b882b16
sharvani801 <sharvanireddy678@gmail.com>
2025-03-18 14:49:41 +0530

    Add files via upload

---
+++ Numbers.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.ArrayList;
+public class Numbers {
+    public static void main(String[] args) {
+        List<String> list=new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+        list.add("5");
+
+        System.out.println("nums");
+        System.out.println("************");
+        System.out.println(list);
+
+    }
+}
