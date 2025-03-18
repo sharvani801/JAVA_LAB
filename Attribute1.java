ace3d2b682b9e815c1956ebe401512c39769d4f8
sharvani801 <sharvanireddy678@gmail.com>
2025-03-18 14:37:22 +0530

    Add files via upload

---
+++ Attribute1.java
@@ -0,0 +1,8 @@
+public class Attribute1 {
+    int x=10;
+    public static void main(String[]args){
+        Attribute1 myObj= new Attribute1();
+        myObj.x=25;//x is now 25
+        System.out.println(myObj.x);
+    }
+}
