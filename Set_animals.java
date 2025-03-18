5a9a29660796714f2cdd01a5952c61e3dbbf7fa0
sharvani801 <sharvanireddy678@gmail.com>
2025-03-18 14:52:57 +0530

    Add files via upload

---
+++ Set_animals.java
@@ -0,0 +1,13 @@
+import java.util.List;
+import java.util.ArrayList;
+public class Set_animals {
+    public static void main(String[]args){
+        Set<String> animal=new HashSet<>(); //creating a set in java
+        animal.add("Dog");
+        animal.add("Cow");
+        animal.add("Cat");
+        animal.add("Pig");
+        animal.add("Elephant");
+        System.out.prinln()
+    }
+}
